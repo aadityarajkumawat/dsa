fc0b22c018a2c05e92736a476112bbe60dd98b46
Aaditya <aaditya01work@gmail.com>
2020-11-08 15:39:38 +0530

    insertion sort

--- arrays/sorting/3-selectionsort.cpp
+++ arrays/sorting/3-selectionsort.cpp
@@ -14,11 +14,20 @@ int main()
 
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
         }
-        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 
     return 0;
