be6cf29fe35c19343d1ce613e43c13e6d740c385
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2020-11-27 15:30:46 +0530

    recursion

---
+++ recursion/2-fibo.cpp
@@ -0,0 +1,27 @@
+/**
+ * Fib(n) = Fib(n-1) + Fib(n-2)
+ * if n = 0 -> return 0
+ * if n = 1 -> return 1
+*/
+
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    cout << fib(n);
+
+    return 0;
+}
