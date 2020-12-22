6b081d1fd55bda3c020539ae656c9175291d51cc
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2020-12-22 10:34:43 +0530

    done paranthesis matching using arrays

--- stackss/paranthesis-matching.cpp
+++ stackss/paranthesis-matching.cpp
@@ -14,6 +14,42 @@ class Stack {
 		char* arr;
 };
 
+int isEmpty(Stack* stack) {
+	if(stack->top == -1) {
+		return 1;
+	}
+	return 0;
+}
+
+int isFull(Stack* stack) {
+	if(stack->top == stack->size - 1) {
+		return 1;
+	}
+	return 0;
+}
+
+void push(Stack* stack, char data) {
+	if(isFull(stack)) {
+		cout << "Stack Overflow" << endl;
+	}
+	else {
+		(stack->top)++;
+		stack->arr[stack->top] = data;
+	}
+}
+
+char pop(Stack* stack) {
+	if(isEmpty(stack)) {
+		cout << "Stack Underflow" << endl;
+		return 'A';
+	}
+	else {
+		char poppedChar = stack->arr[stack->top];
+		(stack->top)--;
+		return poppedChar;
+	}
+}
+
 int main() {
 	Stack *stack = new Stack();
 	
@@ -21,11 +57,24 @@ int main() {
 	stack->top = -1;
 	stack->arr = new char[stack->size];
 	
-	string expression = "(34*12)+22";
+	string expression = "(34)*12+22";
 	char arrayOfExpressionCharcters[expression.length()];
 	for(int i  = 0; i < expression.length(); i++) {
 		arrayOfExpressionCharcters[i] = expression[i];
+		if(expression[i] == '(') {
+			push(stack, expression[i]);
+		}
+		else if(expression[i] == ')') {
+			pop(stack);
+		}
+		else if(expression.length()-1 ==  i) {
+			if(stack->top == -1) {
+				cout << "Paranthesis Matched" << endl;
+			}
+			else {
+				cout << "Paranthesis not matched" << endl;
+			}
+		}
 	}
-
 	return 0;
 }
