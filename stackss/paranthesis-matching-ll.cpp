dbb5987a6727790c77062c7337433d4ae3c87004
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2020-12-24 16:59:48 +0530

    paranthesis matching using stacks and linked list

--- stackss/paranthesis-matching-ll.cpp
+++ stackss/paranthesis-matching-ll.cpp
@@ -34,25 +34,38 @@ Node* push(Node* top, char data) {
 	return top;
 }
 
-char pop(Node* top) {
-
-}
-
-char peek(Node** top, int index) {
-	
+char pop(Node** top) {
+	if(isEmpty(*top)) {
+		return 'A';
+	}
+	Node* temp = *top;
+	char poppedCharcter = (*top)->data;
+	*top = (*top)->next;
+	return poppedCharcter;
 }
 
 int main() {
 	Node* top = NULL;
 	string expression = "(8+5)*6+2";
-	char charcterArray[expression.length()];
 
-	for(int i = 0; i < expression.length(); i++) {
-		charcterArray[i] = expression[i];
-		top = push(top, charcterArray[i]);
-		cout << charcterArray[i];
+	for(int i = 0;i < expression.length(); i++) {
+		if(expression[i] == '(') {
+			top = push(top, expression[i]);
+		}
+		else if(expression[i] == ')') {
+			pop(&top);
+		}
+		else if(expression.length() - 1 == i) {
+			if(top == NULL) {
+				cout << expression << endl;
+				cout << "Paranthesis matched" << endl;
+			}
+			else {
+				cout << expression << endl;
+				cout << "Paranthesis not matched" << endl;
+			}
+		}
 	}
-	cout << endl;
 
 	return 0;
 }
