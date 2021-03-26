8ffe4f6a8334fce542c95ba5d008e4cbd188c06f
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-26 20:11:47 +0530

    bubble sort completed

--- algo/sorting/bubble-sort.cpp
+++ algo/sorting/bubble-sort.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-    std::vector<int>
+void bubbleSort(std::vector<int> &nums) {
+    int n = nums.size();
+    bool swapped = false;
+
+    for(int j = 0;j < n - 1;j++) {
+	swapped = false;
+	for(int i = 0;i < n - j - 1;i++) {
+	    if(nums[i] > nums[i + 1]) {
+		int temp = nums[i];
+		nums[i] = nums[i + 1];
+		nums[i + 1] = temp;
+
+		swapped = true;
+	    }
+	}
+
+	if(swapped == false) {
+	    break;
+	}
+    }
+}
+
+int main()
+{
+    std::vector<int> nums = {5, 1, 4, 2, 8};
+    bubbleSort(nums);
+    for(int i = 0;i < nums.size();i++) {
+	std::cout << nums[i] << std::endl;
+    }
     return 0;
 }
