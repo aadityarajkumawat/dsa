3db15bf19dea1009b46198242cf25bd24acae840
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-27 15:04:35 +0530

    cool so far

--- recursion/5-first-and-last-occurance.cpp
+++ recursion/5-first-and-last-occurance.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
-using namespace std;
 
-int first_occurance(int arr[], int n, int i, int key)
+int firstocc(int arr[], int n, int i, int key)
 {
     if (i == n)
-    {
         return -1;
-    }
 
     if (arr[i] == key)
-    {
         return i;
-    }
-
-    return first_occurance(arr, n, i + 1, key);
+    return firstocc(arr, n, i + 1, key);
 }
 
-int last_occurance(int arr[], int n, int i, int key)
+int lastocc(int arr[], int n, int i, int key)
 {
     if (i == n)
-    {
         return -1;
-    }
-    int restArray = last_occurance(arr, n, i + 1, key);
+
+    int restArray = lastocc(arr, n, i + 1, key);
     if (restArray != -1)
-    {
         return restArray;
-    }
 
     if (arr[i] == key)
-    {
         return i;
-    }
 
     return -1;
 }
@@ -39,7 +28,7 @@ int last_occurance(int arr[], int n, int i, int key)
 int main()
 {
     int arr[] = {4, 2, 1, 2, 5, 2, 7};
-    // cout << first_occurance(arr, 7, 0, 2);
-    cout << last_occurance(arr, 7, 0, 2);
+    std::cout << firstocc(arr, 7, 0, 2) << std::endl;
+    // std::cout << last_occurance(arr, 7, 0, 2);
     return 0;
 }
