ca46365f39ee5015174d176aa0124b6d740a5485
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-31 19:27:38 +0530

    balanced tree

--- trees/balanced-height-tree.cpp
+++ trees/balanced-height-tree.cpp
@@ -15,6 +15,58 @@ struct Node
     }
 };
 
+int height(Node* root) {
+    if (root == NULL) return 0;
+    int lHeight = height(root->left);
+    int rHeight = height(root->right);
+
+    return max(lHeight, rHeight) + 1;
+}
+
+// time complexity - O(n^2)
+bool isBalanced(Node* root) {
+
+    if (root == NULL) return true;
+
+    if (isBalanced(root->left) == false) {
+        return false;
+    }
+    if (isBalanced(root->right) == false) {
+        return false;
+    }
+
+    int lh = height(root->left);
+    int rh = height(root->right);
+
+    if (abs(lh - rh) <= 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// time complexity - O(n)
+bool isBalancedBetter(Node* root, int* height) {
+    if (root == NULL) return true;
+    int lh = 0, rh = 0;
+    if (isBalancedBetter(root->left, &lh) == false) {
+        return false;
+    }
+    if (isBalancedBetter(root->right, &rh) == false) {
+        return false;
+    }
+
+    *height = max(lh, rh) + 1;
+
+    if (abs(lh - rh) <= 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 int main() {
     /*
          1
@@ -33,6 +85,25 @@ int main() {
 
     root->right->left = new Node(6);
     root->right->right = new Node(7);
+
+    Node* root2 = new Node(1);
+    root2->left = new Node(2);
+    root2->left->left = new Node(3);
+
+    // if (isBalanced(root2)) {
+    //     cout << "Balanced tree" << endl;
+    // }
+    // else {
+    //     cout << "Unbalanced tree" << endl;
+    // }
+
+    int height = 0;
+    if (isBalancedBetter(root, &height) == true) {
+        cout << "Balanced Tree" << endl;
+    }
+    else {
+        cout << "Unbalanced Tree" << endl;
+    }
 
     return 0;
 }
