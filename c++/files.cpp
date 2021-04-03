3ebec09c5e4e96d450d122520eb9aebf6c4ab750
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-04-03 16:08:43 +0530

    create dir and file

--- c++/files.cpp
+++ c++/files.cpp
@@ -5,8 +5,9 @@
 
 class FileSystemUtils {
 public:
-    void createDirectory(const char* path) {
-        if (mkdir(path, 0777) == -1) {
+    void create_directory(std::string& path) {
+        const char* type_safe_path = path.c_str();
+        if (mkdir(type_safe_path, 0777) == -1) {
             std::cerr << "Error: " << strerror(errno) << std::endl;
         }
         else {
@@ -14,9 +15,9 @@ public:
         }
     }
 
-    void createFile(std::string fileName) {
+    void create_file(std::string& file_name) {
         std::fstream file;
-        file.open(fileName, std::ios::out);
+        file.open(file_name, std::ios::out);
 
         if (!file) {
             std::cout << "Error in creating file!!" << std::endl;
@@ -31,19 +32,79 @@ public:
 
 class PathUtils {
 public:
-    // std::string join(std::vector<std::string>) {
+    std::string join(std::string& base_path, std::vector<std::string>& parts_of_path) {
+        std::string joint_string = "";
+        const std::string base = trim_slash(base_path, left_trim);
+        joint_string.append(base);
 
-    // }
+        for (int i = 0;i < parts_of_path.size();i++) {
+            if (i != parts_of_path.size() - 1) {
+                joint_string.push_back(SLASH);
+                joint_string.append(trim_slash(parts_of_path[i], trim));
+            }
+            else {
+                joint_string.push_back(SLASH);
+                joint_string.append(trim_slash(parts_of_path[i], left_trim));
+            }
+        }
 
+        return joint_string;
+    }
+
+private:
+    std::string left_trim = "LEFT_TRIM";
+    std::string right_trim = "RIGHT_TRIM";
+    std::string trim = "TRIM";
+    const char SLASH = '/';
+
+    std::string trim_slash(std::string& path, std::string& trim_type) {
+        std::string final_string = "";
+        if (trim_type == left_trim || trim_type == right_trim || trim_type == trim) {
+            std::string temp = path;
+
+            if (trim_type == left_trim) {
+                while (temp != "" && temp[temp.length() - 1] == SLASH) {
+                    temp.pop_back();
+                }
+            }
+            else if (trim_type == right_trim) {
+                while (temp != "" && temp[0] == SLASH) {
+                    temp = temp.substr(1, temp.length());
+                }
+            }
+            else {
+                while (temp != "" && (temp[temp.length() - 1] == SLASH || temp[0] == SLASH)) {
+                    if (temp[temp.length() - 1] == SLASH) {
+                        temp.pop_back();
+                    }
+                    else {
+                        temp = temp.substr(1, temp.length());
+                    }
+                }
+            }
+
+            final_string = temp;
+        }
+
+        return final_string;
+    }
 };
 
 
 int main() {
-    const char* path = "./coolh";
-    FileSystemUtils fs;
+    std::string path = "./coolh";
+    std::string new_file_path = "./coolh/cool.js";
 
-    fs.createDirectory(path);
-    fs.createFile("./coolh/cool.txt");
+    FileSystemUtils fs;
+    PathUtils path_utils;
+
+    fs.create_directory(path);
+    fs.create_file(new_file_path);
+
+    std::string base_path = "/home/edydee///";
+    std::vector<std::string> parts = { "/cool//", "/bin", "slack" };
+
+    std::string pat = path_utils.join(base_path, parts);
 
     return 0;
 }
