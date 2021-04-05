e20759c5c3e28fd6ec6a69503ff871a9a0f4f990
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-04-05 22:27:51 +0530

    i m starting a new repo for this

--- c++/main.cpp
+++ c++/main.cpp
@@ -5,23 +5,34 @@
 // #include <unistd.h>
 // #include <stdio.h>
 
-int main() {
-    std::string path = "./coolh";
-    std::string new_file_path = "./coolh/cool.js";
+int main(int argc, char** argv) {
+    // std::string path = "./coolh";
+    // std::string new_file_path = "./coolh/cool.js";
+
+    // FileSystemUtils fs;
+    // PathUtils path_utils;
+
+    // fs.create_directory(path);
+    // fs.create_file(new_file_path);
+
+    // std::string base_path = "/home/edydee//";
+    // std::vector<std::string> parts = { "/home/all/cood.js" };
+
+    // std::string pat = path_utils.join(base_path, parts);
+    // std::cout << pat << std::endl;
+
+    // fs.create_file(pat);
 
     FileSystemUtils fs;
     PathUtils path_utils;
 
-    fs.create_directory(path);
-    fs.create_file(new_file_path);
+    // for (int i = 0;i < argc;i++) {
+    //     std::cout << argv[i] << std::endl;
+    // }
 
-    std::string base_path = "/home/edydee//";
-    std::vector<std::string> parts = { "/home/all/cood.js" };
-
-    std::string pat = path_utils.join(base_path, parts);
-    std::cout << pat << std::endl;
-
-    fs.create_file(pat);
+    if (!strcmp(argv[1], "init")) {
+        std::cout << "Initiliazed a pit repository" << std::endl;
+    }
 
     return 0;
 }
