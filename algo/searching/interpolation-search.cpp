071f4fb114f19524be5ef2b1f5e4eed6b73a8d49
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-05-15 15:23:19 +0530

    clean

--- algo/searching/interpolation-search.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-
-int main()
-{
-  std::vector<int> nums = {64, 25, 12, 22, 11};
-  return 0;
-}
--- test-folder/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <climits>
-using namespace std;
-
-int main()
-{
-    cout << "Hello from 18" << endl;
-    return 0;
-}
