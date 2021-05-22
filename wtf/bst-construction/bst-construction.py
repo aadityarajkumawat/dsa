31c1b4b6c138b8faec7a0c69f960bd4e2edf4b33
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-05-22 20:37:44 +0530

    did bst construction in py

---
+++ wtf/bst-construction/bst-construction.py
@@ -0,0 +1,118 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Average: O(log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) space
+    def insert(self, value):
+        currentNode = self
+
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+
+        return self
+
+    # Average: O(log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) space
+    def contains(self, value):
+        currentNode = self
+
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+
+        return False
+
+    # Average: O(log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) space
+    def remove(self, value, parentNode=None):
+        currentNode = self
+
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else:
+                # removes any node having non-empty child nodes
+                if currentNode.left is not None and currentNode.right is not None:
+                    currentNode.value = currentNode.right.getMinValue()
+                    currentNode.right.remove(currentNode.value, currentNode)
+                # removes root node if it has only one child
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right = currentNode.right.right
+                    else:
+                        currentNode.value = None
+                # removes a node with a parent and less than 2 child nodes
+                elif parentNode.left == currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+
+                break
+
+        return self
+
+    def getMinValue(self):
+        currentNode = self
+
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+
+        return currentNode.value
+
+    def printBST(self):
+        currentNode = self
+
+        if currentNode.left:
+            currentNode.left.printBST()
+        print(currentNode.value)
+        if currentNode.right:
+            currentNode.right.printBST()
+
+
+# Constructing a BST
+root = BST(10)
+root.insert(8).insert(7).insert(6).insert(11).insert(9).insert(16)
+
+'''
+        10
+       /  \
+      8    11
+     / \    \
+    7   9    16
+   /
+  6
+'''
+
+# root.printBST()
+# print(root.contains(8))
+
+root.remove(7)
+root.printBST()
