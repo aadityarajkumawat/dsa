7c05af33fdcad43194162cc7f8294e6db942c399
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-05-24 00:21:53 +0530

    do linked list in js and cpp

---
+++ wtf/linked-list/linked-list.js
@@ -0,0 +1,29 @@
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // set the given node as head
+    setHead(node) {}
+
+    // set the given node as tail
+    setTail(node) {}
+
+    insertBefore(node, nodeToInsert) {}
+
+    insertAfter(node, nodeToInsert) {}
+
+    insertAtPosition(position, nodeToInsert) {}
+
+    // remove all node with given value
+    removeNodesWithValue(value) {}
+
+    // remove the given particular node
+    remove(node) {}
+
+    // checks if the given value is present in list
+    containsNodeWithValue(value) {}
+}
+
+const list = new DoublyLinkedList();
