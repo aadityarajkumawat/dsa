6a2c3fcab8b31eb0a1c55f345f0c308e71a56182
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-08-02 18:50:56 +0530

    nice

---
+++ Algorithms/validate-subsequence/validateSubsequence.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// bool is_sub_sequence()
+
+int main() {
+    vector<int> arr = { 3, 4, 5, 7, 3, 0, 11 };
+    vector<int> sub_seq = { 3, 7, 0 };
+    return 0;
+}
