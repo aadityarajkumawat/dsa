9a24736a20836d3e5f7a00d2e9516ac5606ef901
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-08-25 00:07:38 +0530

    shifting to java

---
+++ CP/id-and-ship.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void init_code()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+    return 0;
+}
---
+++ CP/little-elephant-and-candies.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void init_code()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+    init_code();
+
+    int test_cases;
+    cin >> test_cases;
+
+    for (int t = 0; t < test_cases; t++)
+    {
+
+        int elephants, candies;
+        cin >> elephants >> candies;
+
+        bool all_elephants_can_be_happy = true;
+
+        for (int i = 0; i < elephants; i++)
+        {
+            int candies_wanted;
+            cin >> candies_wanted;
+            if (candies < candies_wanted)
+            {
+                all_elephants_can_be_happy = false;
+                break;
+            }
+            candies = candies - candies_wanted;
+        }
+
+        if (all_elephants_can_be_happy)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+
+    return 0;
+}
