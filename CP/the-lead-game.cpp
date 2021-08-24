9e4ae9bf49b0c99b4e6d964b56e9d5f01caed7c3
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-08-24 20:44:17 +0530

    solved, the-lead-game

--- CP/the-lead-game.cpp
+++ CP/the-lead-game.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 void init_code() {
@@ -10,9 +10,38 @@ void init_code() {
 
 int main() {
     init_code();
-    int x;
-    cin >> x;
-    cout << x << endl;
+
+    int rounds = 0;
+    cin >> rounds;
+
+    vector<int> a;
+    vector<int> b;
+
+    for(int i = 0; i < rounds; i++) {
+        int f, s;
+        cin >> f >> s;
+        a.push_back(f);
+        b.push_back(s);
+    }
+
+    int winner = 0;
+    int lead = 0;
+
+    for(int i = 0; i < rounds; i++) {
+        a[i] += a[i - 1];
+        b[i] += b[i - 1];
+    }
+
+    for(int i = 0; i < rounds; i++) {
+        int d = abs(a[i] - b[i]);
+        if(d > lead) {
+            lead = d;
+            if(a[i] > b[i]) winner = 1;
+            else winner = 2;
+        }
+    }
+
+    cout << winner << " " << lead << endl;
 
     return 0;
 }
