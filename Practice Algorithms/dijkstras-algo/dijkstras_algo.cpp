6194bdfab81266b8d0cd13e5609b79f84b881f41
Aditya Raj Kumawat <arkumawat78@gmail.com>
2022-01-02 11:06:55 +0530

    cool

---
+++ Practice Algorithms/dijkstras-algo/dijkstras_algo.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DijkstrasAlgo {
+    vector<vector<pair<int, int>>> graph;
+
+    DijkstrasAlgo(vector<vector<pair<int, int>>> _graph) {
+        graph = _graph;
+    }
+};
+
+int main() {
+    return 0;
+}
--- Practice Algorithms/dijkstras-algo/priority_queue.cpp
+++ Practice Algorithms/dijkstras-algo/priority_queue.cpp
@@ -17,22 +17,75 @@ class prioritised_entry {
 };
 
 /*
-    [ { value: 4, priority: 3 }, { value: 1, priority: 5 } ]
+    [ { value: 4, weight: 3 }, { value: 1, weight: 5 } ]
 */
 
 // a priority queue which prioritises smaller interger
-class priority_queue {
+class priority_queue_1 {
     vector<prioritised_entry *> pq;
 
   public:
-    priority_queue() {}
+    priority_queue_1() {}
 
     void add(prioritised_entry *entry) {
+        if (pq.size() == 0) {
+            pq.push_back(entry);
+            return;
+        }
+
+        bool inserted = false;
+
         for (int i = 0; i < pq.size(); i++) {
             prioritised_entry *curr_elem = pq[i];
-            // if (curr_elem->get_weight() <)>
+
+            if (entry->get_weight() < curr_elem->get_weight()) {
+                inserted = true;
+                for (int j = pq.size() - 1; j >= i; j--) {
+                    if (j == pq.size() - 1) {
+                        prioritised_entry* o = pq[j];
+                        pq.push_back(o);
+                    } else {
+                        pq[j + 1] = pq[j];
+                    }
+                }
+
+                pq[i] = entry;
+                return;
+            }
         }
+
+        if (!inserted) {
+            pq.push_back(entry);
+        }
+    }
+
+    prioritised_entry* peek() {
+        return pq[0];
+    }
+
+    void traverse_queue() {
+        for (int i = 0; i < pq.size(); i++) {
+            prioritised_entry* curr = pq[i];
+            std::cout << "{" << curr->get_value() << ", " << curr->get_weight() << "}" << " ";
+        }
+        std::cout << std::endl;
     }
 };
 
-int main() { return 0; }
+int main() { 
+    priority_queue_1* pq = new priority_queue_1();
+    
+    prioritised_entry* pq_e2 = new prioritised_entry(1, 5);
+    pq->add(pq_e2);
+
+    prioritised_entry* pq_e1 = new prioritised_entry(4, 3);
+    pq->add(pq_e1);
+
+    prioritised_entry* pq_e3 = new prioritised_entry(7, 7);
+    pq->add(pq_e3);
+
+
+    pq->traverse_queue();
+
+    return 0;
+}
